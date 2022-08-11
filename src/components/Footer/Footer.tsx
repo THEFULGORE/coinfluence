06b702a156932c79949109e2abc53b495a37370a
THEFULGORE <artur.abuladze@gmail.com>
2022-08-11 14:43:25 +0500

    Footer change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './Footer.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="footer__container">
-        <h2 className="footer__title">Coinfluence</h2>
-        <h3 className="footer__rights">All rights reserved</h3>
-        <div className="footer__links">
-          <Link to="/">Home</Link>
-          <Link to="/cryptocurrencies">Crypto</Link>
-          <Link to="/news">News</Link>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="footer">
+			<div className="footer__container">
+				<h2 className="footer__title">
+					Made with ❤ by{' '}
+					<a href="https://github.com/THEFULGORE" target="_blank">
+						THEFULGORE
+					</a>
+				</h2>
+				<div className="footer__links">
+					<Link to="/">Home</Link>
+					<Link to="/cryptocurrencies">Crypto</Link>
+					<Link to="/news">News</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
