5d8e0096bd153367d759ea7a7657d49e12fbeb55
THEFULGORE <artur.abuladze@gmail.com>
2022-08-11 14:25:20 +0500

    Route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import {
-  Header,
-  Cryptocurrencies,
-  News,
-  Homepage,
-  CryptoDetails,
-  Footer,
-} from "./components";
-import Ticker from "./components/header/ticker/Ticker";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Header, Cryptocurrencies, News, Homepage, CryptoDetails, Footer } from './components';
+import Ticker from './components/header/ticker/Ticker';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <Header />
-        <Ticker />
-      </div>
-      <div className="main">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-          <Route
-            path="/cryptocurrencies/:coinUID"
-            element={<CryptoDetails />}
-          />
-          <Route path="/news" element={<News />} />
-        </Routes>
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="header">
+				<Header />
+				<Ticker />
+			</div>
+			<div className="main">
+				<Routes>
+					<Route path="/*" element={<Homepage />} />
+					<Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+					<Route path="/cryptocurrencies/:coinUID" element={<CryptoDetails />} />
+					<Route path="/news" element={<News />} />
+				</Routes>
+			</div>
+			<div className="footer">
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
